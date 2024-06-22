fef38a022214a49503fcb6845cce53726b8b0c2b
Arpita <157691401+arpita-1155@users.noreply.github.com>
2024-06-22 17:34:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1><b>Arpita Karande</b></h1>
+        <p>IT Developer</p>
+      </header>
+      <section>
+        <h2>I'm an intermediate IT developer with skills in software development.</h2>
+        <p>
+        I have experience with C, C++, and I've worked on few  academic projects . I enjoy solving problems and learning new technologies. I'm looking for a role at a tech company where I can continue to grow and contribute to exciting projects.
+        </p>
+      </section>
+      <section>
+        <h2></h2>
+        <ul>
+          <li>
+            <h3>Project 1</h3>
+            <p>Computer Vision for Gesture Detection</p>
+            <p>
+            Developed a real-time gesture detection system using OpenCV and TensorFlow for intuitive human-computer interaction.
+            </p>
+          </li>
+          <li>
+            <h3>Project 2</h3>
+            <p>Time and Money Management App</p>
+            <p>
+            Currently developing a Time and Money Management App in C, utilizing data structures and algorithms for efficient budget tracking, expense management, and time allocation.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <h3>Government College of Engineering, Karad
+            [Shivaji University of Maharashtra]</h3>
+            <p>Bachelor of Technology Information Technology </p>
+            <p>
+              <b>Relevant Coursework-</b>
+            Data Structure and Algorithm  [DSA], Artificial Intelligence and Machine Learning [AIML], Artificial Intelligence of Things [AIOT].</p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <ul>
+        <li>Technical Skills[C , C++]</li>
+        <li>Team Collaboration</li>
+        <li>Problem-Solving</li>
+        <li>Project Management</li>
+        <li>Communication</li>
+        <li>Time Management</li>
+        <li>Presentation Skills</li>
+        <li>Adaptability</li>
+        </ul>
+      </section>
+      <footer>
+        <p><b>Contact:</b> karandearpita7@gmail.com </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
